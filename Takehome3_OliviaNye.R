b2136dc2fb0431351aa77ea670a73cbdfeacb032
Joshua-Yukich <56749858+Joshua-Yukich@users.noreply.github.com>
2019-11-02 00:51:57 -0500

    added comments, grade to TH3 script

--- Takehome3_OliviaNye.R
+++ Takehome3_OliviaNye.R
@@ -2,20 +2,25 @@
 rm(list = ls())
 cat("\014")  #sends 'CTRL + L' to console oprional
 
-
+# CLOSE BOX ON RHS TOO. 
 #---------------------------------------------------------------------------------------------------#
 #                                                                                                   #
 #   Olivia Nye October 29, 2019 onye@tulane.edu    
-#   Take Home Exercise 3 - Git and R
+#   Take Home Exercise 3 - Git and R 
 #                                                                                                   #
 #   Description: Loads the 'diamonds' built in R data frame and prints the first 6 observations.                                                                                   #
 #                                                                                                   #
 #   Dependencies: ggplot2 library is necessary because the diamonds dataset is in that library                                                                              #
 #---------------------------------------------------------------------------------------------------#
-install.packages("ggplot2")
+#install.packages("ggplot2") DONT INLCUDE THESE LINES IN THE SCRIPT THEY CAUSE PROBLEMS
 library(ggplot2)
 #loading in diamonds
 data(diamonds)
 
 #print first 6 obs
 head(diamonds, 6)
+
+#nicely written proeamble in general, dont inlcude intall packages commands in scripts
+
+# 100% credit. 
+
